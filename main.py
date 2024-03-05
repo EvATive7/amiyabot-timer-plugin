7fabfc7042f34919f9a575acf85690e19be7308e
EvATive7 <evative7@gmail.com>
2024-03-06 02:34:35 +0800

    feat: 支持自定义提醒

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ import asyncio
 import json
 import math
 
+from datetime import datetime
+
+from core.database.plugin import PluginBaseModel
 from core.database.group import GroupBaseModel
 from core.database.messages import *
 from core.util import TimeRecorder
 from core import send_to_console_channel, Message, Chain, AmiyaBotPluginInstance, bot as main_bot, OneBot11Instance
 
 from amiyabot.database import *
+from core.database.bot import Admin
 from amiyabot.network.httpRequests import http_requests
 
 
@@ -18,6 +22,13 @@ class TimerGroupSetting(GroupBaseModel):
     group_id: str = CharField(primary_key=True)
     bot_id: str = CharField(null=True)
     activity_remind: int = IntegerField(default=0, null=True)
+
+
+@table
+class ExtraTimers(PluginBaseModel):
+    name: str = CharField(primary_key=True)
+    time: int = IntegerField(default=0, null=False)
+    info: str = CharField(default='{}', null=True)
 
 
 curr_dir = os.path.dirname(__file__)
@@ -36,7 +47,7 @@ bot = TimerPluginInstance(
     version='1.0',
     plugin_id='arknights-activity-remind',
     plugin_type='',
-    description='通过修改群名片的方式进行事件的倒计时提醒，依赖于插件【明日方舟数据解析】。仅支持LLOnebot。',
+    description='通过修改群名片的方式进行事件的倒计时提醒。',
     document=f'{curr_dir}/README.md',
     global_config_schema=f'{curr_dir}/config_schema.json',
     global_config_default=f'{curr_dir}/config_default.yaml'
@@ -66,6 +77,68 @@ class JsonData:
             cls.cache = {}
 
 
+class PraseDateException(Exception):
+    ...
+
+
+def parse_date(date_str):
+    def try_prase(fstr) -> datetime:
+        nonlocal date_str
+
+        try:
+            return datetime.strptime(date_str, fstr)
+        except:
+            return None
+
+    def complete(dt: datetime, level: int, have_time) -> datetime:
+        now = datetime.now()
+        curyear = now.year
+        curmonth = now.month
+        curday = now.day
+
+        dt = dt.replace(second=0)
+
+        if not have_time:
+            dt = dt.replace(hour=16, minute=0)
+
+        if level >= 1:
+            dt = dt.replace(year=curyear)
+        if level >= 2:
+            dt = dt.replace(month=curmonth)
+        if level >= 3:
+            dt = dt.replace(day=curday)
+
+        if dt < now:
+            if level >= 3:
+                return dt.replace(day=curday+1)
+            if level >= 2:
+                return dt.replace(month=curmonth+1)
+            if level >= 1:
+                return dt.replace(year=curyear+1)
+        else:
+            return dt
+
+    prase_level = [
+        '%Y-%m-%d %H:%M',
+        '%m-%d %H:%M',
+        '%d %H:%M',
+        '%H:%M',
+    ]
+
+    prase_level_without_time = [
+        '%Y-%m-%d',
+        '%m-%d',
+        '%d'
+    ]
+
+    for matcher, have_time in [(prase_level, True), (prase_level_without_time, False)]:
+        for level, fstr in enumerate(matcher):
+            if result := try_prase(fstr):
+                return complete(result, level, have_time).timestamp()
+
+    raise PraseDateException('')
+
+
 @bot.on_message(group_id='remind', keywords=['开启提醒'], level=5)
 async def _(data: Message):
     if not data.is_admin:
@@ -89,6 +162,7 @@ async def _(data: Message):
                 group_id=data.channel_id, bot_id=data.instance.appid, activity_remind=1
             )
 
+    asyncio.create_task(fresh())
     return Chain(data).text('已在本群开启活动提醒')
 
 
@@ -113,6 +187,48 @@ async def _(data: Message):
     return Chain(data).text('已经启动刷新~')
 
 
+@bot.on_message(group_id='remind', keywords=['添加提醒'], level=5)
+async def _(data: Message):
+    if not bool(Admin.get_or_none(account=data.user_id)):
+        return Chain(data).text('抱歉，自定义活动提醒只能由兔兔管理员编辑~')
+
+    try:
+        _, name, time = data.text.split(' ', 2)
+        time = parse_date(time)
+
+        timer: ExtraTimers = ExtraTimers.get_or_none(
+            name=name
+        )
+        if timer:
+            ExtraTimers.update(time=time).where(
+                ExtraTimers.name == name
+            ).execute()
+        else:
+            ExtraTimers.create(
+                name=name, time=time
+            )
+        asyncio.create_task(fresh())
+        return Chain(data).text('成功添加了提醒~')
+    except PraseDateException as e:
+        return Chain(data).text('解析时间字符串失败，添加提醒失败')
+    except Exception as e:
+        return Chain(data).text('添加提醒失败')
+
+
+@bot.on_message(group_id='remind', keywords=['删除提醒'], level=5)
+async def _(data: Message):
+    if not bool(Admin.get_or_none(account=data.user_id)):
+        return Chain(data).text('抱歉，自定义活动提醒只能由兔兔管理员编辑~')
+
+    try:
+        _, name = data.text.split(' ', 2)
+        ExtraTimers.delete().where(ExtraTimers.name == name).execute()
+        asyncio.create_task(fresh())
+        return Chain(data).text('成功删除了提醒~')
+    except Exception as e:
+        return Chain(data).text('删除提醒失败')
+
+
 @bot.timed_task(each=3600, run_when_added=True)
 async def _(_):
     await fresh()
@@ -120,7 +236,9 @@ async def _(_):
 
 async def fresh():
     activity_list = JsonData.get_json_data('activity_table')['basicInfo']
-    consider_list: list[tuple] = []
+    act_timer_list: list[tuple] = []
+    custom_timer_list: list[tuple] = []
+    all_timer_list: list[tuple] = []
 
     for activity in activity_list.values():
         startTime = activity['startTime']
@@ -131,15 +249,21 @@ async def fresh():
         now = time.time()
 
         if startTime > now:
-            consider_list.append((name, startTime))
+            act_timer_list.append((name, startTime))
         if endTime > now:
-            consider_list.append((f'{name}结束', endTime))
+            act_timer_list.append((f'{name}结束', endTime))
         if rewardEndTime > now:
-            consider_list.append((f'{name}商店关闭', endTime))
+            act_timer_list.append((f'{name}商店关闭', endTime))
 
-    if consider_list:
-        consider_list = sorted(consider_list, key=lambda x: x[1])
-        timer_name, timer_time = consider_list[0]
+    for custom_timer in ExtraTimers.select().where(ExtraTimers.time >= now):
+        custom_timer_list.append((custom_timer.name, custom_timer.time))
+
+    act_timer_list = sorted(act_timer_list, key=lambda x: x[1])
+    custom_timer_list = sorted(custom_timer_list, key=lambda x: x[1])
+    all_timer_list = custom_timer_list + act_timer_list
+
+    if all_timer_list:
+        timer_name, timer_time = all_timer_list[0]
         new_nickname_str = '{} | 距{}{}h'.format(bot.get_config('amiyaNickName'), timer_name, int((timer_time-time.time()) / 3600))
     else:
         new_nickname_str = bot.get_config('amiyaNickName')
